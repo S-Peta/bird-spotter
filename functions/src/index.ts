8400f86853dad1aafa535ef45812ead7f915304b
Bero-B <3berob@gmail.com>
2024-08-16 16:24:52 +0100

    testing user endpoint

---
+++ functions/src/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+import * as functions from "firebase-functions";
+
+import * as express from "express";
+
+const app = express();
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+
+exports.app = functions.https.onRequest(app);
