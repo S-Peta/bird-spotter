fc4778f1465f892d0a84544b770caa5d6f9188b3
stefany santos <stefany.chsantos@gmail.com>
2024-08-19 16:23:45 +0100

    endpoints-no-code-api

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore} from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBliqaTr0GYn7Hyg26uqDg3DP1Q8frm0I4",
-  authDomain: "testbirdspotter-db.firebaseapp.com",
-  projectId: "testbirdspotter-db",
-  storageBucket: "testbirdspotter-db.appspot.com",
-  messagingSenderId: "386643740500",
-  appId: "1:386643740500:web:4b9ecf4091e2840373aac1",
-  measurementId: "G-SJGE3Y0FDH"
+  apiKey: "AIzaSyA9ySyi3QPJwmgtB9xG8fdzFP0U2LP6Pt8",
+  authDomain: "birdspotter-db.firebaseapp.com",
+  projectId: "birdspotter-db",
+  storageBucket: "birdspotter-db.appspot.com",
+  messagingSenderId: "1055905898244",
+  appId: "1:1055905898244:web:f5be6d18b3f91287bffa51",
+  measurementId: "G-179352B53B"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
+export const firebase_auth = getAuth(app)
--- utils/getData.ts
+++ utils/getData.ts
@@ -1,10 +1,29 @@
 import { collection, getDocs } from 'firebase/firestore';
 
-export async function getUsers(db:any, collectionName:string) {
-    const usersCol = collection(db, collectionName)
-    const usersSnapshot = await getDocs(usersCol)
-    const usersList = usersSnapshot.docs.map((doc) => {
-      return doc.data()
-    })
-    return usersList
-  }
+export async function getUsers(db:any) {
+  const usersCol = collection(db, 'Users')
+  const usersSnapshot = await getDocs(usersCol)
+  const usersList = usersSnapshot.docs.map((doc) => {
+    return doc.data()
+  })
+  return usersList
+}
+
+export async function getCaughtBirds(db:any) {
+  const caugthBirdsCol = collection(db, 'Caught Birds')
+  const caugthBirdsSnapshot = await getDocs(caugthBirdsCol)
+  const caugthBirdsList = caugthBirdsSnapshot.docs.map((doc) => {
+    return doc.data()
+  })
+  return caugthBirdsList
+}
+
+export async function getBirds(db:any) {
+  const birdsCol = collection(db, 'birds')
+  const birdsSnapshot = await getDocs(birdsCol)
+  const birdsList = birdsSnapshot.docs.map((doc) => {
+    return doc.data()
+  })
+  return birdsList
+}
+
