185f2e9e2ce87991462351eb1062b1cf4f2c9df0
enterthestream <zaid9010@gmail.com>
2024-08-19 09:44:27 +0100

    add function for uploading bird images to firebase storage

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
-
 import { initializeApp } from "firebase/app";
-import { getFirestore} from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBliqaTr0GYn7Hyg26uqDg3DP1Q8frm0I4",
   authDomain: "testbirdspotter-db.firebaseapp.com",
   projectId: "testbirdspotter-db",
-  storageBucket: "testbirdspotter-db.appspot.com",
+  storageBucket: "gs://test-885b4.appspot.com",
   messagingSenderId: "386643740500",
   appId: "1:386643740500:web:4b9ecf4091e2840373aac1",
-  measurementId: "G-SJGE3Y0FDH"
+  measurementId: "G-SJGE3Y0FDH",
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+
+export const db = getFirestore(app);
+export const storage = getStorage();
