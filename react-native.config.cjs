da67eaf5c15a21d22f8e5efd88d536b4e679d4cf
enterthestream <zaid9010@gmail.com>
2024-08-23 14:55:02 +0100

    add function requestpermissions to allow access to user media

--- react-native.config.cjs
+++ react-native.config.cjs
@@ -5,9 +5,11 @@ module.exports = {
   },
   assets: ["./src/assets/"],
   dependencies: {
-    'react-native-tensorflow': {
+    "react-native-tensorflow": {
       platforms: {
         ios: null, // Disable iOS platform if you're linking manually or using CocoaPods
         android: null, // Disable Android platform if linking manually
-  }}
+      },
+    },
+  },
 };
