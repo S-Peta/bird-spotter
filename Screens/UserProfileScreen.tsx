54e71f9f7929a7d1aec98cd56047799010216173
stefany santos <stefany.chsantos@gmail.com>
2024-08-23 17:02:58 +0100

    setup profile

--- Screens/UserProfileScreen.tsx
+++ Screens/UserProfileScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet, Pressable } from 'react-native'
+import { View, Text, Button, StyleSheet, Pressable, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { firebase_auth } from '../index';
 import { signOut, getAuth } from "firebase/auth";
@@ -10,6 +10,8 @@ import { getPointsForUser } from '../utils/getData';
 import { useContext } from 'react';
 import { PointsContext } from '../Contexts/Points';
 import { Link } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 const UserProfileScreen = () => {
   const auth = firebase_auth;
@@ -40,30 +42,63 @@ const UserProfileScreen = () => {
       })
     }
   }, [])
-  
+
 
   const progress = totalCaughtBirds / totalBirds
+  const defaultImageUri = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.momscleanairforce.org%2Fwhy-are-birds-falling-from-the-sky%2F&psig=AOvVaw1zLug5whErn4frZLnVL5KX&ust=1724514272548000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJi1qcO6i4gDFQAAAAAdAAAAABAj'
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>User Profile</Text>
-      <Text style={styles.text}>Your Progress</Text>
-      <Progress.Bar style={styles.progressBar} progress={progress} width={300} color="#4caf50"/>
-      <Text style={styles.progressText}>{totalCaughtBirds}/{totalBirds} birds caught</Text>
-      <Text style={styles.pointsText}>Total Points: {points}</Text>
-      <Link to={{screen: 'Ranking'}} style={styles.link}>
-        <Text style={styles.button}>View Rankings</Text>
-      </Link>
-      <Link to={{screen: 'Caught Birds', params: {totalCaughtBirds, totalBirds, userId: user?.uid, progress}}} style={styles.link}>
-        <Text style={styles.button}>Your Caught Birds</Text>
-      </Link>
-     <Pressable style={styles.logoutButton} onPress={LogOut}>
-        <Text style={styles.logoutButtonText}>Log out</Text>
-      </Pressable>
-    
-    </View>
-  
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: 'white'
+    }}>
+      <StatusBar backgroundColor='gray'/>
+      <View style={{width: '100%'}}>
+        <Image
+        source={{ uri: defaultImageUri }}
+        resizeMode='cover'
+        style={{
+          height: 228,
+          width: '100%'
+        }}
+        />
+      </View>
+
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <Image
+        source={{uri: user.avatar }}
+        resizeMode='contain'
+        style={{
+          height: 155,
+          width: 155,
+          borderRadius: 999,
+          borderColor: 'green',
+          marginTop: -90
+        }}/>
+      </View>
+
+      <Text></Text>
+    </SafeAreaView>
   )
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Text style={styles.headerText}>User Profile</Text>
+  //     <Text style={styles.text}>Your Progress</Text>
+  //     <Progress.Bar style={styles.progressBar} progress={progress} width={300} color="#4caf50"/>
+  //     <Text style={styles.progressText}>{totalCaughtBirds}/{totalBirds} birds caught</Text>
+  //     <Text style={styles.pointsText}>Total Points: {points}</Text>
+  //     <Link to={{screen: 'Ranking'}} style={styles.link}>
+  //       <Text style={styles.button}>View Rankings</Text>
+  //     </Link>
+  //     <Link to={{screen: 'Caught Birds', params: {totalCaughtBirds, totalBirds, userId: user?.uid, progress}}} style={styles.link}>
+  //       <Text style={styles.button}>Your Caught Birds</Text>
+  //     </Link>
+  //     <Pressable style={styles.logoutButton} onPress={LogOut}>
+  //       <Text style={styles.logoutButtonText}>Log out</Text>
+  //     </Pressable>
+  //   </View>
+  // )
 }
 const styles = StyleSheet.create({
   container: {
@@ -136,4 +171,3 @@ const styles = StyleSheet.create({
 });
 
 export default UserProfileScreen
-
