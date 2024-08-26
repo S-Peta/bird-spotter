9f8e40077f83494e90f13a995ae6a4117d903e4f
Eunwoo Kim <153867711+KimEunwooDev@users.noreply.github.com>
2024-08-26 21:59:09 +0100

    add feature navigation to Guess Page

--- Screens/CameraScreen.tsx
+++ Screens/CameraScreen.tsx
@@ -1,12 +1,120 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  Modal,
+  Pressable,
+} from "react-native";
+import React from "react";
+import { useState, useEffect } from "react";
+import { updateUserTwentyPoints } from "../utils/updateUserPoints";
 
-const CameraScreen = () => {
+const CameraScreen = ({ navigation }) => {
+  let prediction = "EASTERN YELLOW ROBIN";
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View>
-      <Text>Camera</Text>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Predict the Bird</Text>
+      </Pressable>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Guess the bird?</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                navigation.navigate("Guess Page", {
+                  predictedBird: prediction,
+                });
+                updateUserTwentyPoints();
+              }}
+            >
+              <Text style={styles.textStyle}>Yes</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                navigation.navigate("Result Page", {
+                  predictedBird: "BLACK FRANCOLIN",
+                });
+                updateUserTwentyPoints();
+              }}
+            >
+              <Text style={styles.textStyle}>No</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
-  )
-}
+  );
+};
 
-export default CameraScreen
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 10,
+    color: "#555",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    margin: 5,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
+
+export default CameraScreen;
