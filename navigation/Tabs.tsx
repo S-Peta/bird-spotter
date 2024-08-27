f6d41f2bf766e6d1bb05df0a0f2ae2069775554c
stefany santos <stefany.chsantos@gmail.com>
2024-08-27 16:10:02 +0100

    CaughtBirdsScreen

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MapScreen from "../Screens/MapScreen";
-import CameraScreen from "../Screens/CameraScreen";
 import UserProfileScreen from "../Screens/UserProfileScreen";
 import { PointsProvider } from "../Contexts/Points";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -10,11 +9,12 @@ import CaughtBirdsScreen from "../Screens/CaughtBirdsScreen";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { View, Text } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import Feather from "@expo/vector-icons/Feather";
 import SingleBirdScreen from "../Screens/SingleBirdScreen";
 import GuessPage from "../Screens/GuessPage";
 import ResultPage from "../Screens/ResultPage";
 import PredictionPage from "../Screens/CameraPredictionScreen";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Entypo from '@expo/vector-icons/Entypo';
 
 const Tab = createBottomTabNavigator();
 const HomeStack = createNativeStackNavigator();
@@ -30,7 +30,7 @@ export default function Tabs() {
           options={{ headerShown: true }}
         /> */}
         <Tab.Screen
-          name="Home"
+          name="User Profile"
           options={{
             headerShown: false,
             tabBarShowLabel: false,
@@ -39,12 +39,10 @@ export default function Tabs() {
                 <View
                   style={{ alignItems: "center", justifyContent: "center" }}
                 >
-                  <AntDesign
-                    name="home"
-                    size={24}
-                    color={focused ? "red" : "#162478"}
+                  <FontAwesome name="user" size={24}
+                    color={focused ? "orange" : "black"}
                   />
-                  <Text style={{ fontSize: 12, color: "#162478" }}>Home</Text>
+                  {/* <Text style={{ fontSize: 12, color: "#162478" }}>Home</Text> */}
                 </View>
               );
             },
@@ -68,6 +66,7 @@ export default function Tabs() {
             </HomeStack.Navigator>
           )}
         </Tab.Screen>
+
         <Tab.Screen
           name="Map"
           component={MapScreen}
@@ -78,17 +77,36 @@ export default function Tabs() {
                 <View
                   style={{ alignItems: "center", justifyContent: "center" }}
                 >
-                  <Feather
-                    name="map-pin"
-                    size={24}
-                    color={focused ? "red" : "#162478"}
+                  <Ionicons name="map" size={24}
+                    color={focused ? "orange" : "black"}
                   />
-                  <Text style={{ fontSize: 12, color: "#162478" }}>Map</Text>
+                  {/* <Text style={{ fontSize: 12, color: "#162478" }}>Map</Text> */}
                 </View>
               );
             },
           }}
         />
+
+        <Tab.Screen
+          name="Caught Birds"
+          component={CaughtBirdsScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View
+                  style={{ alignItems: "center", justifyContent: "center" }}
+                >
+                  <Entypo name="book" size={24}
+                    color={focused ? "orange" : "black"}
+                  />
+                  {/* <Text style={{ fontSize: 12, color: "#162478" }}>Map</Text> */}
+                </View>
+              );
+            },
+          }}
+        />
+
         <Tab.Screen
           name="Camera Page"
           options={{
@@ -102,14 +120,15 @@ export default function Tabs() {
                   <AntDesign
                     name="camera"
                     size={24}
-                    color={focused ? "red" : "#162478"}
+                    color={focused ? "orange" : "black"}
                   />
-                  <Text style={{ fontSize: 12, color: "#162478" }}>Camera</Text>
+                  {/* <Text style={{ fontSize: 12, color: "#162478" }}>Camera</Text> */}
                 </View>
               );
             },
           }}
         >
+
           {() => (
             <CameraStack.Navigator>
               <CameraStack.Screen
